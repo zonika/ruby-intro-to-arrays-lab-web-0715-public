121c1c34533af11efa142e3248150c5a11771af9
msuzoagu <msuzoagu@gmail.com>
2015-04-05 01:07:04 -0400

    update tests

--- spec/intro_to_arrays_spec.rb
+++ spec/intro_to_arrays_spec.rb
@@ -1,4 +1,6 @@
+require 'pry'
 describe "working with arrays" do
+
   context 'instantiating' do
 
     it "creates an empty array and assigns the array to a variable" do
@@ -7,30 +9,32 @@ describe "working with arrays" do
       expect(__).to be_an(Array)
     end
 
-    context 'instantiating an array with 2 objects' do
+    context 'instantiating an array - named answer -  with 2 objects' do
       # create your array here
+      # your array should be set to equal a local variable named answer
 
       it "is an array" do
-        # replace the underscore in expect with your variable
-        expect(__).to be_an(Array)
+        expect(answer).to be_an(Array)
       end
 
       it "has 2 objects in it" do
-        # replace the underscore in expect with your variable
-        expect(__).to_not eq([])
+        expect(answer).to_not eq([])
       end
     end
   end
 
   context 'indexing' do
-    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    before(:all) do
+      @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    end
 
     it "returns the first element in the array using its positive index" do
-      answer = __ # replace the underscore with your answer
+      # answer = __ # replace the underscore with your answer
       expect(answer).to eq("Welcome to New York")
     end
 
     it "returns the third element in the array using its positive index" do
+      # answer = taylor_swift[2]
       answer = __ # replace the underscore with your answer
       expect(answer).to eq("Style")
     end
@@ -47,10 +51,12 @@ describe "working with arrays" do
   end
 
   context 'using ruby array methods to return values from an array' do
-    south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    before(:all) do
+      @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+    end
 
     it 'gets the first element in the array ' do
-      answer = __ # replace the underscore with your answer
+      # answer = __ # replace the underscore with your answer
       expect(answer).to eq("Thailand")
     end
 
@@ -62,9 +68,11 @@ describe "working with arrays" do
   end
 
   context 'using ruby array methods to get information about an array' do
-    it 'returns the length of the array' do
-      programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+    before(:all) do
+      @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+    end
 
+    it 'returns the length of the array' do
       answer = __ # replace the underscore with your answer
       expect(answer).to eq(8)
     end
