e949474d724380eba5c5aceaa8cd3ab5d5a53763
zonika <zchodosh@conncoll.edu>
2015-07-30 14:25:10 -0400

    done

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  arr=Array.new
+end
+def array_with_two_elements
+  arr=[1,2]
+end
+def first_element(arr)
+  arr[0]
+end
+def third_element(arr)
+  arr[2]
+end
+def last_element(arr)
+  arr[-1]
+end
+def first_element_with_array_methods(arr)
+  arr.shift
+end
+def last_element_with_array_methods(arr)
+  arr.pop
+end
+def length_of_array(arr)
+  arr.length
+end
